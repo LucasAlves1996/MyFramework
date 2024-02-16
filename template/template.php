f99c158fa282ad386b00819814bdd8e4105cea3a
Lucas Costa Alves <lucas.alves263@gmail.com>
2024-02-16 12:07:05 -0300

    Criação de método para inserção de dados no banco de dados além de métodos auxiliares para o mesmo. Foram excluídos alguns diretórios e classes que não estavam sendo utilizados.

--- template/template.php
+++ template/template.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="template/css/app.css">
+    <link rel="stylesheet" href="template/css/style.css">
   </head>
   <body>
     <header>
@@ -58,11 +58,12 @@
         </div>
         <div style="background-color:#212326;color:rgb(200,200,200);">
           <h5 style="width:100%;text-align:center;margin:0;padding:23px 0px 20px 0px;">
-            Desenvolvido por <a href="https://www.lucasdeveloper.com.br/" style="outline:none;">Lucas Costa Alves</a>
+            Desenvolvido por <a href="https://linkedin.com/in/lucas-costa-alves/" style="outline:none;">Lucas Costa Alves</a>
           </h5>
         </div>
     </footer>
 
+    <script src="template/js/script.js" />
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- vendor/MyFramework/Model/Model.php
+++ vendor/MyFramework/Model/Model.php
@@ -10,6 +10,61 @@ abstract class Model
     {
         $this->db = $db;
     }
+
+    public function insert($objectTable)
+    {
+        $table = str_replace("app\\models\\", "", strtolower(get_class($objectTable)));
+        $columns = $this->getColumns($objectTable);
+        $values = implode(', ', array_fill(0, $this->getValuesLength($objectTable), '?'));
+        
+        $sql = "INSERT INTO $table ($columns) VALUES ($values)";
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($this->getValues($objectTable));
+        return $this->db->lastInsertId();
+    }
+
+    public function getColumns($objectTable)
+    {
+        $reflectionClass = new \ReflectionClass(get_class($objectTable));
+        $attributes = $reflectionClass->getProperties(\ReflectionProperty::IS_PRIVATE);
+
+        $attributesNames = [];
+        foreach ($attributes as $attribute) {
+            $attributesNames[] = $attribute->getName();
+        }
+
+        $stringAttributes = implode(', ', $attributesNames);
+
+        return $stringAttributes;
+    }
+
+    public function getValuesLength($objectTable)
+    {
+        $reflectionClass = new \ReflectionClass(get_class($objectTable));
+        $attributes = $reflectionClass->getProperties(\ReflectionProperty::IS_PRIVATE);
+
+        $attributesValuesLength = 0;
+        foreach ($attributes as $attribute) {
+            $attributesValuesLength++;
+        }
+
+        return $attributesValuesLength;
+    }
+
+    public function getValues($objectTable)
+    {
+        $reflectionClass = new \ReflectionClass(get_class($objectTable));
+        $attributes = $reflectionClass->getProperties(\ReflectionProperty::IS_PRIVATE);
+
+        $attributesValues = [];
+        foreach ($attributes as $attribute) {
+            $attribute->setAccessible(true);
+            $attributesValues[] = $attribute->getValue($objectTable);
+        }
+
+        return $attributesValues;
+    }
 }
 
 ?>
